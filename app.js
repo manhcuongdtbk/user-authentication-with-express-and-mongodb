a3b2cd568daa9e9b92f4ae4fcf8b4c19dee727a0
manhcuongdtbk <manhcuongdtbk@gmail.com>
2019-08-03 12:19:18 +0700

    Add pugjs view engine

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ const app = express();
 // Parse incoming request
 express.json();
 express.urlencoded({ extended: false });
+
+// View engine setup
+app.use('view engine', 'pug');
+app.set('views', __dirname + '/views');
